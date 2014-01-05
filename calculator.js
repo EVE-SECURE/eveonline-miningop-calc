c4b023dd447677ba36791b1801950d45ee43198d
Andrew Spano <andrew@andrews-comp.(none)>
2014-01-04 23:47:01 -0500

    Calculate function: changed dividend to total ship man hours instead of operation length

--- calculator.js
+++ calculator.js
@@ -1,3 +1,6 @@
+var form_count = 1;
+var player_amount = 1; //the total amount of players who particpated
+var ship_hours = 1;
 function modify_height(tableid) {
     //Modifies the number of rows of the table
 
@@ -17,10 +20,29 @@ function modify_height(tableid) {
 
 function add_player(formid) {
     var form = document.getElementById(formid);
-    document.writeln("<br>");
-    form.createElement("input");
-}
-function calculate(hours_contributed, total_value, number_of_players) {
+    //var new_player = new FieldSet("andrew");
     
 }
 
+
+
+function calculate(hours_contributed, op_length, total_value) {
+    /* abstract out actual calculation logic, create seperate function
+    for form manipulation */
+    var hour_worth = total_value / (ship_hours * 1.00) ; //parens to guarantee float division
+    alert(hour_worth);
+    return hours_contributed * hour_worth;
+}
+
+
+function form_calculate(output_form) {
+    if( output_form === undefined) {
+	output_form = document.getElementById("rewardfield");
+    }
+    var op_length = parseInt(document.getElementById("durationfield").value);
+
+    var hours_contrib = parseInt(document.getElementById("hoursfield").value);
+    ship_hours = ship_hours + hours_contrib;
+    var total_val = parseInt(document.getElementById("totalfield").value);
+    output_form.value = calculate(hours_contrib, op_length, total_val);
+}
