2ea87d2c029d0d8e6145a6f9316c3998b96bd048
Andrew Spano <andrew@andrews-comp.(none)>
2014-01-05 11:48:59 -0500

    deleted useless modify_height function

--- calculator.js
+++ calculator.js
@@ -68,28 +68,11 @@ function create_table(){
 }
 
 
-function modify_height(tableid) {
-    //Modifies the number of rows of the table
-
-    var table = document.getElementById(tableid);
-    var row_count = table.rows.length;
-    var row = table.insertRow(row_count);
-    var col_count = table.rows[0].cells.length;
-    
-    var id_count = 1;
-/*    for(var c = 0; c < col_count; c = c + 1) {
-	var newcell = row.insertCell(c);
-	newcell.innerHTML = table.rows[row_count-1].cells[c].innerHTML;
-	newcell.setAttribute("id",table.rows[row_count-1].cells[c].id + 1 );
-	alert(newcell.id);
-	}*/
-}
-
 function add_player() {
-    var player = new Player(document.getElementById("namefield"), parseInt(document.getElementById("hoursfield")))
+    var player = new Player(document.getElementById("namefield").value, parseInt(document.getElementById("hoursfield").value))
     alert(player);
     players.add_player(player);
-    alert(player);
+    alert(player.name + " " + player.hours_contributed);
     document.getElementById("namefield").value = "";
     document.getElementById("hoursfield").value = 0.00;
 
